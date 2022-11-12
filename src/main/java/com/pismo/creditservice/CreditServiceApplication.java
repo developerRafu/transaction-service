58c83b4e384d04d99ea0f84868d0a31d564ae74b
rafael <7aecitl42ksw4odjny6bthxfhbib6ilug5lozeh2fowjycgurfnq>
2022-11-12 18:30:55 -0300

    [open-api] - adding open api.

--- src/main/java/com/pismo/creditservice/CreditServiceApplication.java
+++ src/main/java/com/pismo/creditservice/CreditServiceApplication.java
@@ -1,9 +1,12 @@
 package com.pismo.creditservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Credit-service API", version = "1.0", description = "Credit Information"))
 public class CreditServiceApplication {
 
     public static void main(String[] args) {
