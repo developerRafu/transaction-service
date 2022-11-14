19eabfe3c703a5a7b60e06cd459ee994fbf99610
rafael <rafael.henrique@techlead.com.br>
2022-11-14 11:46:42 -0300

    [technical-debits-v2] - Adding validations for requests.

---
+++ src/main/java/com/pismo/creditservice/errors/RestExceptionHandler.java
@@ -0,0 +1,68 @@
+package com.pismo.creditservice.errors;
+
+import com.pismo.creditservice.errors.helpers.MessagesEnum;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@RestController
+public class RestExceptionHandler {
+
+    @ExceptionHandler(InvalidDocumentNumberException.class)
+    public ResponseEntity<DefaultError> handleInvalidDocumentNumberException(final InvalidDocumentNumberException ex, final WebRequest request) {
+        final var error = DefaultError
+                .builder()
+                .message(MessagesEnum.INVALID_NUMBER_DOCUMENT.getFormattedMessage(ex.getMessage()))
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<DefaultError> handleAccountNotFoundException(final AccountNotFoundException ex, final WebRequest request) {
+        final var error = DefaultError
+                .builder()
+                .message(MessagesEnum.ACCOUNT_NOT_FOUND.getFormattedMessage(ex.getAccountId()))
+                .code(HttpStatus.NOT_FOUND.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DefaultError> handleMethodArgumentNotValidException(final MethodArgumentNotValidException ex, final WebRequest request) {
+        final var error = DefaultError
+                .builder()
+                .message(MessagesEnum.INVALID_REQUEST.getFormattedMessage())
+                .details(getMessages(ex))
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+
+    @ExceptionHandler(TransactionTypeNotFound.class)
+    public ResponseEntity<DefaultError> handleInvalidDocumentNumberException(final TransactionTypeNotFound ex, final WebRequest request) {
+        final var error = DefaultError
+                .builder()
+                .message(MessagesEnum.INVALID_TRANSACTION_TYPE.getFormattedMessage(ex.getTransactionTypeId()))
+                .code(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
+    }
+
+    private List<String> getMessages(MethodArgumentNotValidException exception) {
+        return exception
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(f -> MessagesEnum.INVALID_VALUE.getFormattedMessage(f.getField(), f.getRejectedValue()))
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/com/pismo/creditservice/services/AccountServiceImpl.java
+++ src/main/java/com/pismo/creditservice/services/AccountServiceImpl.java
@@ -1,13 +1,11 @@
 package com.pismo.creditservice.services;
 
 import com.pismo.creditservice.domain.Account;
-import com.pismo.creditservice.errors.InvalidDocumentNumberException;
 import com.pismo.creditservice.repositories.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -17,7 +15,7 @@ public class AccountServiceImpl implements IAccountService {
 
     @Override
     public Account create(final String documentNumber) {
-        validateDocumentNumber(documentNumber);
+        //validateDocumentNumber(documentNumber);
         final var account = Account.builder().documentNumber(documentNumber).build();
         return repository.save(account);
     }
@@ -27,9 +25,9 @@ public class AccountServiceImpl implements IAccountService {
         return repository.findById(accountId);
     }
 
-    private void validateDocumentNumber(final String documentNumber) {
-        if (Objects.isNull(documentNumber) || documentNumber.length() < 11) {
-            throw new InvalidDocumentNumberException(documentNumber);
-        }
-    }
+//    private void validateDocumentNumber(final String documentNumber) {
+//        if (Objects.isNull(documentNumber) || documentNumber.length() < 11) {
+//            throw new InvalidDocumentNumberException(documentNumber);
+//        }
+//    }
 }
